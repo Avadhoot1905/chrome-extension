505bd5e74d72da8267877e9b7f956cdd0a2c4b7c
unknown <arcsmo19@gmail.com>
2025-01-25 15:29:05 +0530

    feat: the extension shows random images now

---
+++ script.js
@@ -0,0 +1,16 @@
+async function dataFectch(){
+    var number = Math.floor(Math.random() * 1084) + 1;
+    fetch(`https://picsum.photos/id/${number}/200/300`)
+    .then(response => response.blob())
+    .then(blob => {
+        const img = document.createElement('img');
+        img.src = URL.createObjectURL(blob);
+        document.body.appendChild(img);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+    console.log("Working");
+}
+
+dataFectch()
